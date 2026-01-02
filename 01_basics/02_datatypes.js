09d7eeeb346467d09150ba73569a4df9b466a07f
Swastika Satya <swastika22102@iiitnr.edu.in>
2026-01-02 14:41:20 +0530

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as new version
+
+// alert("hello");// We are using Nodejs not browser
+
+
+console.log(3+3); 
+console.log("Swastika");
+
+let age = 24;
+let relationShip = true;
+let name = "Swastika";
+let colorTone = null;
+
+console.log(typeof null);
+//type of null is an object
+
+console.log(typeof undefined);
+
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
-console.log("Swastika");
+const accountId = 221020453;
+const accountName = "Swastika";
+const accountIdString = "221020453";
+
+let accountEmail = "swastika2180@gmail.com";
+var accountCity = "Raipur";
+let account;
+console.log(accountEmail);
+
+console.table([accountId, accountEmail,accountName]);
+
